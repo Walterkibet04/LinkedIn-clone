9da79ccd7db953c1439626b9f580211820f04eb8
Walterkibet04 <134695229+Walterkibet04@users.noreply.github.com>
2023-10-10 12:28:01 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let profileMenu = document.getElementById('profile-menu')
+
+function toggleMenu(){
+    profileMenu.classList.toggle("open-menu")
+}
+
+let sidebarActivity = document.getElementById("sidebarActivity");
+
+let showMore = document.getElementById("showMoreLink");
+
+function toogleActivity(){
+    sidebarActivity.classList.toggle("open-activity");
+
+    
+}
